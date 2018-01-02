ec9df3fc1d6eb23cdda6e06516a590a3a7060006
Nastenka <nastenka9410@gmail.com>
2018-01-02 10:20:17 +0200

    hello world

---
+++ src/components/Feed/index.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import Styles from './styles';
+// import { v4 } from 'uuid';
+
+// INSTRUMENTS
+// import {
+//     CSSTransition,
+//     Transition,
+//     TransitionGroup
+// } from 'react-transition-group';
+// import { fromTo } from 'gsap';
+// import Styles from './styles';
+
+// COMPONENTS
+
+
+export default class Feed extends Component {
+
+
+    render () {
+
+        return (
+            <div className = { Styles.feed }>
+                Hello, world!
+            </div>
+        );
+    }
+}
---
+++ src/containers/App/index.js
@@ -0,0 +1,16 @@
+// Core
+import React, { Component } from 'react';
+
+// Instruments
+import Feed from '../../components/Feed';
+import Catcher from '../../components/Catcher';
+
+export default class App extends Component {
+    render () {
+        return (
+            <Catcher>
+                <Feed />
+            </Catcher>
+        );
+    }
+}
